472f5773b77efca5346d1b1ebc2bbfae096a5582
snapo <snapo@users.noreply.github.com>
2019-09-20 16:44:48 +0200

    rounding missmatch
    still catching on the ceil instead of floor (bug!?!??)

--- numbergen.py
+++ numbergen.py
@@ -10,16 +10,15 @@ hashobj = hashlib.sha256('Hello World'.encode('utf-8'))
 numberslist = []
 start = time.time()
 # Looperino starting here
-for i in range(1000000):
+for i in range(10000):
     number = 0
     hashobj = hashlib.sha256(hashobj.hexdigest().encode('utf-8'))
     intversion = int(hashobj.hexdigest()[0:13], 16) # Javascript Original | parseInt(hash.slice(0,52/4),16);
     if (intversion % 101 == 0): # Check if Divisible by 101, if yes put out immediately 1 so users loose (1% house edge)
-        number = 1.0000
+        number = 1.000000000
     else:
         b = 4503599627370496 # Javascript Original | Math.pow(2,52);
-        #number = math.floor((100 * b - intversion) / (b - intversion)) / 100
-        number = math.ceil((100.0000 * b - intversion) / (b - intversion)) / 100.0000
+        number = math.ceil(((100 * b - intversion) / (b - intversion) / 100) * 100) / 100
     numberslist.append(number)
     #print(number) ### PLEASE PLEASE if doing bigger tests comment this out! 20x speed increase
 end = time.time()
@@ -33,14 +32,6 @@ resultmedian = statistics.median(numberslist)
 resultmedian_low = statistics.median_low(numberslist)
 resultmedian_high = statistics.median_high(numberslist)
 resultmedian_grouped = statistics.median_grouped(numberslist)
-resultmode = statistics.mode(numberslist)
-
-resultpstdev = statistics.pstdev(numberslist)
-resultpvariance = statistics.pvariance(numberslist)
-resultstdev = statistics.stdev(numberslist)
-resultvariance = statistics.variance(numberslist)
-
-
 end2 = time.time()
 
 
@@ -48,17 +39,11 @@ print("----------------------------------")
 print("Time required Bust numbers: " + str(end - start))
 print("Time required for Statistics: " + str(end2 - start2))
 
-print("Highest Value: " + str(resulthighest))
+print("Highest Number: " + str(resulthighest))
+
 print("Arithmetic mean (“average”) of data: " + str(resultmean))
 print("Harmonic mean of data: " + str(resultharmonic_mean))
 print("Median (middle value) of data: " + str(resultmedian))
 print("Low median of data: " + str(resultmedian_low))
 print("High median of data: " + str(resultmedian_high))
 print("Median, or 50th percentile, of grouped data: " + str(resultmedian_grouped))
-print("Mode (most common value) of discrete data: " + str(resultmode))
-
-print("Population standard deviation of data: " + str(resultpstdev))
-print("Population variance of data: " + str(resultpvariance))
-print("Sample standard deviation of data: " + str(resultstdev))
-print("Sample variance of data: " + str(resultvariance))
-
